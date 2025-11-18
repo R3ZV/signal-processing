2a6e69a13a770db12d975ef0ad630850c48b0770
R3ZV <razvan.maracine@protonmail.com>
2025-11-18 23:19:39 +0200

    feat: Lab6 ex4

--- lab6/ex4.py
+++ lab6/ex4.py
@@ -1,4 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-    N = 100
+def main():
+    N = 20
+    for d in np.random.randint(1, 19, 10):
+        x = np.random.rand(N)
+        y = np.roll(x, shift=d)
 
-    x = np.random.rand(N)
+        d_rev1 = np.fft.ifft(np.fft.fft(x).conj() * np.fft.fft(y))
+        d_rev2 = np.fft.ifft(np.fft.fft(y) / np.fft.fft(x))
+
+        print(f"Real d: {np.argmax(d_rev1)}")
+        print(f"Method1 d: {np.argmax(d_rev1)}")
+        print(f"Method2 d: {np.argmax(d_rev2)}")
+        print("--------------------------------")
+
+main()
