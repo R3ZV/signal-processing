1d53fbc2e26fabd5056caaa5b19862b10a75e588
R3ZV <razvan.maracine@protonmail.com>
2025-12-10 08:44:03 +0200

    feat: Ex2 lab9

--- lab9/main.py
+++ lab9/main.py
@@ -37,8 +37,29 @@ def exp2(signal, alpha, beta):
 
     return pred, err
 
-def exp3(signal, alpha, beta):
-    return [], 0
+def exp3(signal, alpha, beta, sigma):
+    L = 3
+    pred = np.zeros(len(signal))
+    s = np.zeros(len(signal))
+    b = np.zeros(len(signal))
+    c = np.zeros(len(signal))
+
+    s[0] = signal[0]
+    b[0] = signal[1] - signal[0]
+    c[0] = 0
+
+    err = 0.0
+    for i in range(L, len(s) - 1):
+        s[i] = alpha * signal[i] + (1 - alpha) * (s[i - 1] + b[i - 1])
+        b[i] = beta * (s[i] - s[i - 1]) + (1 - beta) * b[i - 1]
+        c[i] = sigma * (signal[i] - s[i] - b[i - 1] + (1 - sigma) * c[i - L])
+
+    m = 1
+    for t in range(len(s) - m):
+        pred[t + m] = s[t] + m * b[t] + c[t - L + 1 + (m - 1) % L]
+        err += (pred[t + m] - signal[t + m]) ** 2
+
+    return pred, err
 
 def main():
     N = 1000
@@ -54,7 +75,7 @@ def main():
     axs[0].plot(time, signal)
     axs[0].set_title("Original")
 
-    alphas = np.linspace(0, 1, 50)
+    alphas = np.linspace(0, 1, 20)
     min_err = 999999
     best_alpha = 0
     for i in range(len(alphas)):
@@ -69,7 +90,7 @@ def main():
     axs[1].set_title("Exp 1")
     print("Exp1: err={}, best_alpha={}".format(err, alphas[best_alpha]))
 
-    betas = np.linspace(0, 1, 50)
+    betas = np.linspace(0, 1, 20)
     min_err = 999999
     best_alpha = 0
     best_beta = 0
@@ -89,9 +110,29 @@ def main():
     axs[2].set_title("Exp 2")
     print("Exp2: err={}, best_alpha={}, best_beta={}".format(err, alphas[best_alpha], betas[best_beta]))
 
-    # pred, err = exp3(signal, alpha, beta)
-    # axs[3].plot(time, pred)
-    # axs[3].set_title("Exp 3")
+    sigmas = np.linspace(0, 1, 20)
+    min_err = 999999
+    best_alpha = 0
+    best_beta = 0
+    best_sigma = 0
+    for i in range(len(alphas)):
+        alpha = alphas[i]
+        for j in range(len(betas)):
+            beta = betas[j]
+            for k in range(len(sigmas)):
+                sigma = sigmas[k]
+
+                pred, err = exp3(signal, alpha, beta, sigma)
+                if err < min_err:
+                    min_err = err
+                    best_alpha = i
+                    best_beta = j
+                    best_sigma = k
+
+    pred, err = exp3(signal, alphas[best_alpha], betas[best_beta], sigmas[best_sigma])
+    axs[3].plot(time, pred)
+    axs[3].set_title("Exp 3")
+    print("Exp3: err={}, best_alpha={}, best_beta={}, best_sigma={}".format(err, alphas[best_alpha], betas[best_beta], sigmas[best_sigma]))
 
     plt.savefig("images/ex2.pdf")
     plt.show()
